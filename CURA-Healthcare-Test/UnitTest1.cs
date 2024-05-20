3a53e91e1a2f71371da055d4891eb8d5f56c321a
mSifuentesDev <msifuentes.dev@gmail.com>
2024-05-20 09:45:55 -0700

    Add project files.

---
+++ CURA-Healthcare-Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CURA_Healthcare_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
